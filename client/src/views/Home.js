20c038efaf7d347976cdf80fc1f15eca18e21f94
Daniel <dandesz198@gmail.com>
2018-12-14 12:29:55 +0100

    init

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,10 +1,51 @@
-import React, { Component, Fragment } from 'react';
-import { view } from 'react-stax'
+import React, { Component, Fragment } from "react";
+import { view } from "react-stax";
+import styled from "styled-components";
+import img from '../assets/bg.png'
+
+const Container = styled.div`
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-size: 30vw;
+  background-color: #eee;
+  padding: 5vh 5vw;
+  text-align: left;
+
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+`;
+
+const Heading = styled.h1`
+  font-size: 36px;
+  color: #222;
+  font-weight: 800;
+`;
+
+const Subheading = styled.h2`
+  font-size: 28px;
+  color: #444;
+  font-weight: 400;
+`;
+
+const Image = styled.img`
+  width: 30vw;
+`;
 
 class Home extends Component {
-    render() {
-        return(<h1>Welcome there! This is going to be the home page of the app.</h1>)
-    }
+  render() {
+    return (
+      <Container>
+        <Image alt="Logo" src={require("../assets/logo_trans.png")} />
+        <Heading>Pass.</Heading>
+        <Subheading>
+          Purchase your Budapest-area tickets and passes with ease.
+        </Subheading>
+      </Container>
+    );
+  }
 }
 
-export default view(Home)
+export default view(Home);
